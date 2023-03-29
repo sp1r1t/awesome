1f91a637185cbac58a2b07d1582b8b451c0ff8bb
Julian Konrath <julian.konrath@gmail.com>
2023-03-29 19:37:55 +0200

    switch to tripple monitor setup

--- scripts/screensetup.sh
+++ scripts/screensetup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-xrandr --output DisplayPort-0 --mode 2560x1440 --pos 3840x0 --rotate right --output HDMI-A-0 --primary --mode 3840x2160 --pos 0x240 --rotate normal --output DVI-D-0 --off
+#xrandr --output DisplayPort-0 --mode 2560x1440 --pos 3840x0 --rotate right --output HDMI-A-0 --primary --mode 3840x2160 --pos 0x240 --rotate normal --output DVI-D-0 --off
+
+xrandr --output DisplayPort-0 --mode 2560x1440 --pos 7095x153 --rotate right --output HDMI-A-0 --primary --mode 3840x2160 --pos 3255x304 --rotate normal --output DVI-D-0 --mode 2560x1440 --pos 695x586 --rotate normal
